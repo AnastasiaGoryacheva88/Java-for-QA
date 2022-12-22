da6c4d7888f143f85a33dd362e8311d5c07b333d
Anastasia test <gorandi@inbox.ru>
2022-12-22 19:06:35 +0300

    add DZ21

---
+++ src/DZ21.java
@@ -0,0 +1,8 @@
+public class DZ21 {
+    public static void main(String[] args) {
+         int price = 50;
+         int mile=20;
+         int milesAccrual= price/mile;
+         System.out.println(milesAccrual);
+    }
+}
