0879c304c79fb90cc43e58d2039431a4d28e1696
Anastasia test <gorandi@inbox.ru>
2022-12-26 16:07:27 +0300

    new DZ22

--- src/DZ22.java
+++ src/DZ22.java
@@ -3,10 +3,13 @@ public class DZ22 {
         int customersStartAccount = 100;
         int depositAmount = 11000;
         int startBonus;
-        if (depositAmount>=1000)
-        {startBonus=depositAmount/100;}
-        else {startBonus=0;}
-        int balance = customersStartAccount+depositAmount+startBonus;
-        System.out.println("Ваш баланс:"+balance+"руб.Ваш бонус:"+startBonus+"руб.");
-    }}
+        if (depositAmount >= 1000) {
+            startBonus = depositAmount / 100;
+        } else {
+            startBonus = 0;
+        }
+        int balance = customersStartAccount + depositAmount + startBonus;
+        System.out.println("Ваш баланс:" + balance + "руб.Ваш бонус:" + startBonus + "руб.");
+    }
+}
 
