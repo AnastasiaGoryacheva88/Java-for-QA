f7cb7c0be85bd24ea285adbf35ddb8c10d2cce8f
Anastasia test <gorandi@inbox.ru>
2022-12-26 17:51:49 +0300

    new DZ31

--- src/DZ31.java
+++ src/DZ31.java
@@ -1,7 +1,7 @@
 public class DZ31 {
     public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
-        int milesAccrual = service.calculate(60);
+        int milesAccrual = service.calculate(80);
         System.out.println(milesAccrual);
     }
 }
